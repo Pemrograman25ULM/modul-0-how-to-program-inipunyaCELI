9f1625b18e1b196327b9dd68c7a72b5e0dd0c7aa
inipunyaCELI <543221158@smktelkom-bjb.sch.id>
2025-09-24 09:26:11 +0800

    File Soal Praktikum no 2

---
+++ soal_2/PRAK002-2510817120019-Gt. Qowita Celia Aurellizha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char nama[] = "Gt. Qowita Celia Aurellizha";
+
+    printf("Selamat Pagi, %s\n", nama);
+    printf("Selamat Siang, %s\n", nama);
+    printf("Selamat Malam, %s\n", nama);
+
+    return 0;
+}
---
+++ soal_2/PRAK002-2510817120019-Gt. Qowita Celia Aurellizha.py
@@ -0,0 +1,5 @@
+nama = "Gt. Qowita Celia Aurellizha"
+
+print("Selamat Pagi,", nama)
+print("Selamat Siang,", nama)
+print("Selamat Malam,", nama)
